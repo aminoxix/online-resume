efd2808e23d4b0a547e1d5f9a58f98d50ede1234
Anshumaan Kumar Prasad <75872316+amino19@users.noreply.github.com>
2021-10-13 21:56:18 +0000

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+/*
+ *  Using strict case for index.js with jQuery.
+ *  Created On 14 October 2021
+ */
+
+(function ($) {
+    "use strict";
+
+    // Loader
+    $(function () {
+        var loader = function () {
+            setTimeout(function () {
+                if ($('#loader').length > 0) {
+                    $('#loader').removeClass('show');
+                }
+            }, 1);
+        };
+        loader();
+    });
+
+    // Auto Init 
+    M.AutoInit();
+
+})(jQuery);
+
+console.clear();
